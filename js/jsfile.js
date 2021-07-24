b49acf31a50fe1e87d15e061ef1d7bf8425d9482
Amjad15 <amj85.95@gmail.com>
2021-07-25 02:26:21 +0300

    Js update

--- js/jsfile.js
+++ js/jsfile.js
@@ -1,12 +1,31 @@
 function savepost (){
 
-    var title=document.getElementById('title');
-    var post=document.getElementById('post');
-
+    var title=document.getElementById('title').value;
+    var today = new Date();
+    
+    var day=today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+    localStorage.setItem("name",document.getElementById('Name').value);
     localStorage.setItem("title",title);
-    localStorage.setItem("post",psot);
+    localStorage.setItem("day",day);
+    localStorage.setItem("IsPosted",1);
 
 
 }
 
+function getlast (){
 
+   
+    
+    if ( localStorage.getItem("name") ){
+        document.getElementById('lastName').innerText=localStorage.getItem("name");
+        document.getElementById('lastPost').innerText=localStorage.getItem("title");
+        document.getElementById('lastDay').innerText=localStorage.getItem("day");
+    }
+    if(localStorage.getItem("IsPosted") !=0){
+
+        document.getElementById('success').style.display="block";
+        localStorage.setItem("IsPosted",0);
+    }
+
+
+}
