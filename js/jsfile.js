30cbc9bdf4bd45b235dbbc97e937cb6ad332ea50
Amjad15 <amj85.95@gmail.com>
2021-07-25 01:47:38 +0300

    js files

---
+++ js/jsfile.js
@@ -0,0 +1,12 @@
+function savepost (){
+
+    var title=document.getElementById('title');
+    var post=document.getElementById('post');
+
+    localStorage.setItem("title",title);
+    localStorage.setItem("post",psot);
+
+
+}
+
+
